c017dcf17f87ba070470561d8d49dbda32343f42
ITsAnnaSky <AnnaDePlane@yandex.ru>
2023-06-03 21:58:00 +0500

    Поправка линтера

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "env": {
+      "browser": true,
+      "es2021": true,
+      "node": true
+  },
+  "extends": "eslint:recommended",
+  "overrides": [
+  ],
+  "parser": "@babel/eslint-parser",
+  "parserOptions": {
+      "ecmaVersion": "latest",
+      "sourceType": "module"
+  },
+  "rules": {
+  }
+}
